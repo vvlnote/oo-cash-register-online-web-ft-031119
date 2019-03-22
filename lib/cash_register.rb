71a668e1e7fa021a2228779f1510df0a5a6c05ac
Joy Lin <vvlnote@hotmail.com>
2019-03-22 22:10:48 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,6 +13,6 @@ class CashRegister
   
   def apply_discount
     self.total -= (self.total * self.discount/100)
-    "After the discount, the total comes to #{@total}."
+    "After the discount, the total comes to $#{@total}."
   end
 end
