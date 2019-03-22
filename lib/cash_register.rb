b63bac031b5f108823f6689c509ba8736209e558
Joy Lin <vvlnote@hotmail.com>
2019-03-22 21:00:47 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
-
+class CashRegister
+  
+  attr_accessor :total, :discount
+  
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+  end
+  
+  def add_item(item, price, quantity=1)
+    self.total += (price * quantity)
+  end
+  
+  def apply_discount
+    self.total -= (self.total * self.discount)
+  end
+end
